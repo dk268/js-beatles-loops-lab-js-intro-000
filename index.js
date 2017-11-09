cf1d203741a045cf1a0b798388ff04db63a504b9
Daniel Kerr <lorddanp@aol.com>
2017-11-09 06:07:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ function theBeatlesPlay(musicianArray, instrumentArray) {
 
 
 function johnLennonFacts(factsArray){
-  factsArray = [
+  /*factsArray = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"
-  ];
+];*/
   var i = 0;
   while (i < factsArray.length) {
     factsArray[i] = `${factsArray[i]}!!!`
