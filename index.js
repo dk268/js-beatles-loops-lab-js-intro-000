d3087eabf9c0411d708a8955fad5daa4b22eabad
Daniel Kerr <lorddanp@aol.com>
2017-11-09 05:54:55 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay(musicianArray, instrumentArray) {
+  var emptyArray = [];
+  for (var i = 0; i < musicianArray.length; i++) {
+    emptyArray[i] = "John Lennon plays guitar"
+    return emptyArray;
+  }
+}
