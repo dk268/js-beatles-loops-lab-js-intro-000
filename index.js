c08d500a7a73d2933eefb03e47bcc2e69921b380
Daniel Kerr <lorddanp@aol.com>
2017-11-09 06:00:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,26 @@ function theBeatlesPlay(musicianArray, instrumentArray) {
     return emptyArray;
   }
 }
+
+
+
+function johnLennonFacts(factsArray){
+  factsArray = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+  ];
+  var i = 0;
+  while (i < factsArray.length) {
+    factsArray[i] = `${factsArray[i]}!!!`
+    i++
+  }
+}
+
+function iLoveTheBeatles (number){
+  var vapid = [];
+  do {vapid[number]="I love the Beatles!";
+      number++;}
+    while (number < 15);
+  }
